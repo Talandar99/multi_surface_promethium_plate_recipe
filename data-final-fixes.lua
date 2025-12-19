33047ac946d22078777a67fcb1b1db507491d6fb
Talandar <talandar66@protonmail.com>
2025-12-19 11:26:57 +0100

    i think it's done

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,43 +1,58 @@
-local placeholder = "msppr-input-placeholder"
-local first_real_input = "promethium-asteroid-chunk"
+local first_real_input = "msppr-crushed-promethium"
 
--- Zbieramy wszystkie receptury mające placeholder
-local chain = {}
+local msppr_chain = {}
 
 for _, recipe in pairs(data.raw.recipe) do
-	if recipe.ingredients then
-		for _, ing in ipairs(recipe.ingredients) do
-			if ing.name == placeholder then
-				table.insert(chain, recipe)
-				break
-			end
-		end
+	if recipe.msppr and recipe.name ~= "msppr-hot-promethium-plate" and recipe.name ~= "msppr-promethium-plate" then
+		table.insert(msppr_chain, recipe)
 	end
 end
--- Możesz posortować, jeśli chcesz narzucić kolejność:
--- table.sort(chain, function(a, b) return a.name < b.name end)
 
-local last_output = nil
+table.sort(msppr_chain, function(a, b)
+	return a.msppr.recipe_chain_order < b.msppr.recipe_chain_order
+end)
 
-for i, recipe in ipairs(chain) do
-	local ingredients = recipe.ingredients
+table.insert(msppr_chain, data.raw.recipe["msppr-hot-promethium-plate"])
+table.insert(msppr_chain, data.raw.recipe["msppr-promethium-plate"])
 
-	-- Wybieramy input
-	local new_input = (i == 1) and first_real_input or last_output
+local tech = data.raw.technology["promethium-science-pack"]
+table.insert(tech.effects, {
+	type = "unlock-recipe",
+	recipe = "msppr-crushed-promethium",
+})
+for _, recipe in ipairs(msppr_chain) do
+	table.insert(tech.effects, {
+		type = "unlock-recipe",
+		recipe = recipe.name,
+	})
+end
 
-	-- Zamieniamy placeholder na aktualny input
-	for _, ing in ipairs(ingredients) do
-		if ing.name == placeholder then
-			ing.name = new_input
+local last_item = first_real_input
+
+for _, recipe in ipairs(msppr_chain) do
+	local res = recipe.msppr.result
+	local amount = res.amount or 1
+
+	table.insert(recipe.ingredients, {
+		type = "item",
+		name = last_item,
+		amount = amount,
+		ignored_by_stats = amount,
+		ignored_by_productivity = amount,
+	})
+
+	recipe.results = recipe.results or {}
+
+	for _, r in pairs(recipe.results) do
+		if r.name == res.name then
+			r.amount = amount
+			r.ignored_by_stats = amount
+			r.ignored_by_productivity = amount
+			break
 		end
 	end
-
-	-- Pobieramy nazwę outputu tej receptury
-	local results = recipe.results
-	if not results or not results[1] then
-		log("Recipe " .. recipe.name .. " has no results!")
-		break
-	end
-
-	last_output = results[1].name
+	recipe.allow_productivity = false
+	recipe.auto_recycle = false
+	last_item = res.name
+	recipe.icon = nil
 end
---
+++ data-updates.lua
@@ -0,0 +1,5 @@
+data.raw["recipe"]["promethium-science-pack"].ingredients = {
+	{ type = "item", name = "biter-egg", amount = 1 },
+	{ type = "item", name = "quantum-processor", amount = 1 },
+	{ type = "item", name = "msppr-promethium-plate", amount = 25 },
+}
