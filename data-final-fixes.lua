963866699831406fb59dc21a7f72eed052c6aba9
Talandar <Talandar66@protonmail.com>
2025-12-22 17:23:13 +0100

    space spidertron

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,3 +1,6 @@
+-------------------------------------------------------------------------------
+--- building chain
+-------------------------------------------------------------------------------
 local first_real_input = "msppr-crushed-promethium"
 
 local msppr_chain = {}
@@ -55,4 +58,76 @@ for _, recipe in ipairs(msppr_chain) do
 	recipe.allow_productivity = false
 	recipe.auto_recycle = false
 	last_item = res.name
+end
+-------------------------------------------------------------------------------
+--- override promethium recipes
+-------------------------------------------------------------------------------
+for _, recipe in pairs(data.raw.recipe) do
+	local produces_promethium_science = false
+
+	if recipe.result == "promethium-science-pack" then
+		produces_promethium_science = true
+	end
+
+	if recipe.results then
+		for _, r in pairs(recipe.results) do
+			if r.name == "promethium-science-pack" then
+				produces_promethium_science = true
+				break
+			end
+		end
+	end
+
+	if produces_promethium_science and recipe.ingredients then
+		for _, ing in pairs(recipe.ingredients) do
+			if ing.name == "promethium-asteroid-chunk" then
+				ing.name = "msppr-promethium-plate"
+			end
+		end
+	end
+end
+-------------------------------------------------------------------------------
+--- spidertron compatibility
+-------------------------------------------------------------------------------
+if settings.startup["space-spidertron-compatibility"].value then
+	if mods["space-spidertron"] then
+		local promethium_science_packs_unit_ingredients =
+			table.deepcopy(data.raw["technology"]["research-productivity"].unit.ingredients)
+		data:extend({
+			{
+				type = "technology",
+				name = "msppr-space-spidertron",
+				icon = "__space-spidertron__/graphics/space-spidertron/space-spidertron-icon.png",
+				icon_size = 64,
+				effects = {
+					{
+						type = "unlock-recipe",
+						recipe = "ss-space-spidertron",
+					},
+				},
+				prerequisites = { "promethium-science-pack" },
+				unit = {
+					count = 2000,
+					ingredients = promethium_science_packs_unit_ingredients,
+					time = 60,
+				},
+			},
+		})
+
+		local tech = data.raw.technology["spidertron"]
+
+		if tech and tech.effects then
+			for i = #tech.effects, 1, -1 do
+				local effect = tech.effects[i]
+				if effect.type == "unlock-recipe" and effect.recipe == "ss-space-spidertron" then
+					table.remove(tech.effects, i)
+				end
+			end
+		end
+		table.insert(data.raw["recipe"]["ss-space-spidertron"].ingredients, {
+			type = "item",
+			name = "msppr-promethium-plate",
+			amount = 50,
+		})
+	end
 end
--- data-updates.lua
+++
@@ -1,5 +0,0 @@
-data.raw["recipe"]["promethium-science-pack"].ingredients = {
-	{ type = "item", name = "biter-egg", amount = 1 },
-	{ type = "item", name = "quantum-processor", amount = 1 },
-	{ type = "item", name = "msppr-promethium-plate", amount = 25 },
-}
